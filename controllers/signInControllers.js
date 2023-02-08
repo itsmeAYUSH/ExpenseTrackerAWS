ab8719d5d9d0e8dae3f0ddd59a23dcefb5709e05
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2023-02-08 23:51:58 +0530

    Make the backend api of the login
    Make the backend api of the login

--- controllers/signInControllers.js
+++ controllers/signInControllers.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const Users = require("../models/users");
 
 const rootDir = require("../util/rootDir.js");
 
@@ -9,8 +10,19 @@ exports.getSignInData = (req, res, next) => {
 exports.postSignInData = (req, res, next) => {
   const signInData = {
     email: req.body.email,
-    password: req.body.password
+    password: req.body.password,
   };
   console.log(signInData);
-  res.send('<script>alert("Logged In successfully")</script>');
+  Users.findAll().then((users) => {
+    const emailExistance = users.find((user) => user.email === req.body.email);
+    if (emailExistance) {
+      if (emailExistance.password === req.body.password) {
+        res.send("<h3>Logged In successfully</h3>");
+      } else {
+        res.status(401).send("<h3>Incorrect password.. Try again</h3>");
+      }
+    } else {
+      res.status(404).send("<h3>No Email Found</h3>");
+    }
+  });
 };
